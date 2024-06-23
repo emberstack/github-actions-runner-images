41a4cb257689c6f3b0c0c031284ccf06ec90e530
Romeo Dumitrescu <romeo@drc.ro>
2024-06-23 20:17:27 +0300

    refactor(ci): rename and restructure GitHub Actions runner images
    - Renamed the base image build step from "build - base image" to "build - ubuntu - base".
    - Changed working directory for building the base image from "./src/images/base" to "./src/images/ubuntu/base".
    - Renamed the build-and-push step from "build-and-push - default" to "build-and-push - ubuntu".
    - Added new tags for pushing: ":ubuntu" and ":ubuntu-latest".
    - Moved 'build-base.ps1' script from 'src/images/base' to 'src/images/ubuntu/base'.
    - Updated docker build command in 'build-base.ps1' script, changed tag name from 'actions-runner-base' to 'github-actions-runner:ubuntu-base'.
    - Moved Dockerfile from 'src/images/base' to 'src/images/ubuntu/base'.
    - Moved Dockerfile from 'src/images/default' to 'src/images/ubuntu/default', updated FROM directive in Dockerfile, changed base image name from 'actions-runner-base' to 'github-actions-runner:ubuntu-base'.

--- src/images/ubuntu/base/build-base.ps1
+++ src/images/ubuntu/base/build-base.ps1
@@ -8,4 +8,4 @@ Write-Output "runner-container-hooks version: $runnerContainerHooksVersion"
 $buildxVersion= (Invoke-WebRequest -Uri "https://github.com/docker/buildx/releases/latest" -UseBasicParsing).BaseResponse.RequestMessage.RequestUri.Segments[-1].Replace("v","")
 Write-Output "buildx version: $buildxVersion" 
 
-docker build --build-arg RUNNER_VERSION=$runnerVersion --build-arg RUNNER_CONTAINER_HOOKS_VERSION=$runnerContainerHooksVersion --build-arg BUILDX_VERSION=$buildxVersion -t actions-runner-base .
+docker build --build-arg RUNNER_VERSION=$runnerVersion --build-arg RUNNER_CONTAINER_HOOKS_VERSION=$runnerContainerHooksVersion --build-arg BUILDX_VERSION=$buildxVersion -t github-actions-runner:ubuntu-base .
